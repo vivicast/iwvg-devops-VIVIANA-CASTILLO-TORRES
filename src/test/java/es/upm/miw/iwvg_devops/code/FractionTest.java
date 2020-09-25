8688472eb2a0c5b2cddb1e3e61facd008b2b8078
Viviana Castillo <v.castillo@alumnos.upm.es>
2020-09-25 11:23:03 +0200

    new class FractionTest #7

---
+++ src/test/java/es/upm/miw/iwvg_devops/code/FractionTest.java
@@ -0,0 +1,56 @@
+package es.upm.miw.iwvg_devops.code;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FractionTest {
+
+    private Fraction fraction;
+
+    @BeforeEach
+    void before() {
+        this.fraction = new Fraction(4, 2);
+    }
+
+    @Test
+    void testFraction() {
+
+        Fraction fract = new Fraction();
+        assertEquals(1, fract.getNumerator());
+        assertEquals(1, fract.getDenominator());
+
+    }
+
+    @Test
+    void testFractionIntInt() {
+
+        assertEquals(4, this.fraction.getNumerator());
+        assertEquals(2, this.fraction.getDenominator());
+
+    }
+
+    @Test
+    void testDecimal() {
+
+        assertEquals(2.0, this.fraction.decimal(), 10e-5);
+
+
+    }
+
+    @Test
+    void testSetNumerator() {
+
+        this.fraction.setNumerator(1);
+        assertEquals(1, this.fraction.getNumerator());
+    }
+
+    @Test
+    void testDenominator() {
+
+        this.fraction.setDenominator(1);
+        assertEquals(1, this.fraction.getDenominator());
+    }
+
+}
