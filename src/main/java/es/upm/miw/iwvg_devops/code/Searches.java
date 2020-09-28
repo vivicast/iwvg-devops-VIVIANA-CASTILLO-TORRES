188af0772c63088c6bd09b4bb029b5b814763c2c
Viviana Castillo <v.castillo@alumnos.upm.es>
2020-09-28 14:58:54 +0200

    Second search issue #13

--- src/main/java/es/upm/miw/iwvg_devops/code/Searches.java
+++ src/main/java/es/upm/miw/iwvg_devops/code/Searches.java
@@ -2,10 +2,17 @@ package es.upm.miw.iwvg_devops.code;
 
 import java.util.stream.Stream;
 
+/**
+ * primeros cuatro valores distintos del último commit realizado de la liberación anterior: 3150
+ * 0 Stream<String> findUserFamilyNameInitialByAnyProperFraction();
+ * 1 Stream<String> findUserIdByAnyProperFraction();
+ * 3 Fraction findFractionDivisionByUserId(String id);
+ * 5 Stream<String> findUserIdByAllProperFraction();
+ */
 public class Searches {
 
 
-    public Stream<String> findUserFamilyNameInitialByAnyProperFraction(){
+    public Stream<String> findUserFamilyNameInitialByAnyProperFraction() {
         return new UsersDatabase().findAll()
                 .filter(user -> user.getFamilyName().startsWith("B"))
                 .filter(user -> user.getFractions().stream()
@@ -13,5 +20,11 @@ public class Searches {
                 .map(User::fullName);
     }
 
+    public Stream<String> findUserIdByAnyProperFraction() {
+        return new UsersDatabase().findAll()
+                .filter(user -> user.getFractions().stream()
+                        .anyMatch(fraction -> fraction.isProper(fraction)))
+                .map(User::getId);
 
+    }
 }
